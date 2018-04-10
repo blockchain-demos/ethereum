7f26047f749aa9bd3454d8088a0b85e6f2c2e3c5
Jesus <chuyqa@gmail.com>
2018-04-10 00:23:52 -0700

    Ethereum Projects Layout cleanup

--- 0_Intro-Interacting_with_the_ethereum_chain/dockerscripts/start-nb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-jupyter lab \
---NotebookApp.ip=0.0.0.0 \
---NotebookApp.token='ethdemos' \
---NotebookApp.iopub_msg_rate_limit=10000 \
---NotebookApp.port=8895 \
---FileManagerMixin.use_atomic_writing=False
